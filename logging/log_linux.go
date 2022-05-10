9a17c1b444e1e9f32d550144c01008c8ff5457e7
junqirao <jackrabbit872568318@aliyun.com>
2022-05-10 15:28:51 +0800

    debug日志输出控制

--- logging/log_linux.go
+++ logging/log_linux.go
@@ -15,10 +15,14 @@ func getConfigString(logPah string) string {
 	if logPah != "" {
 		file = "        <rollingfile type=\"size\" filename=\"" + file + "\" maxsize=\"102400\" maxrolls=\"5\"/>\n"
 	}
+	debug := ""
+	if version.Develop {
+		debug = ",debug"
+	}
 	return "" +
 		"<seelog type=\"sync\">\n" +
 		"    <outputs formatid=\"main\">\n" +
-		"        <filter levels=\"trace,debug,info\">\n" +
+		"        <filter levels=\"trace,info" + debug + "\">\n" +
 		"            <console formatid=\"colored-default\"/>\n" +
 		"        </filter>\n" +
 		"        <filter levels=\"warn\">\n" +
