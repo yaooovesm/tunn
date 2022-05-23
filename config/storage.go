a39e1161f2a2507ba8280429e755ab3b396befb7
junqirao <jackrabbit872568318@aliyun.com>
2022-05-23 17:50:14 +0800

    配置读写

---
+++ config/storage.go
@@ -0,0 +1,77 @@
+package config
+
+import (
+	"encoding/json"
+	log "github.com/cihub/seelog"
+	"io/ioutil"
+	"os"
+)
+
+//
+// ClientConfigStorage
+// @Description:
+//
+type ClientConfigStorage struct {
+	User     User     `json:"user"`
+	Auth     Auth     `json:"auth"`
+	Security Security `json:"security"`
+	Admin    Admin    `json:"admin"`
+}
+
+//
+// ReadFromFile
+// @Description:
+// @receiver cfg
+// @param path
+//
+func (cfg *ClientConfigStorage) ReadFromFile(path string) {
+	if path == "" {
+		_ = log.Error("config not specific")
+		os.Exit(-1)
+		return
+	}
+	log.Info("load config from : ", path)
+	file, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+	if err != nil {
+		_ = log.Error("failed to open config file : " + err.Error())
+		os.Exit(-1)
+	}
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		_ = log.Error("failed to read config file : " + err.Error())
+		os.Exit(-1)
+	}
+	_ = json.Unmarshal(bytes, cfg)
+}
+
+//
+// ToConfig
+// @Description:
+// @receiver cfg
+// @return Config
+//
+func (cfg *ClientConfigStorage) ToConfig() Config {
+	return Config{
+		User:     cfg.User,
+		Auth:     cfg.Auth,
+		Security: cfg.Security,
+		Admin:    cfg.Admin,
+	}
+}
+
+//
+// Dump
+// @Description:
+// @receiver cfg
+//
+func (cfg *ClientConfigStorage) Dump(path string) error {
+	bytes, err := json.MarshalIndent(cfg, "", "    ")
+	if err != nil {
+		return err
+	}
+	log.Info("config dump to : ", path)
+	return ioutil.WriteFile(path, bytes, 0600)
+}
