592d3e527fc01bcc8565ad4d3d0f15c130420dc6
junqirao <jackrabbit872568318@aliyun.com>
2022-05-24 17:15:26 +0800

    拆分admin用户

--- config/admin.go
+++ config/admin.go
@@ -1,12 +1,39 @@
 package config
 
+import "errors"
+
 //
 // Admin
 // @Description:
 //
 type Admin struct {
-	Address  string `json:"address"`
-	Port     int    `json:"port"`
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+	AdminUser
+}
+
+//
+// AdminUser
+// @Description:
+//
+type AdminUser struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
+}
+
+//
+// Check
+// @Description:
+// @receiver u
+// @param user
+// @return error
+//
+func (u *AdminUser) Check(user AdminUser) error {
+	if u.Password != user.Password {
+		return errors.New("密码错误")
+	}
+	if u.User != user.User {
+		return errors.New("用户错误")
+	}
+	return nil
 }
