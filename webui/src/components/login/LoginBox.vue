8ff57de73e27b5dda77161cd2cbd48b9061c39fe
junqirao <jackrabbit872568318@aliyun.com>
2022-05-24 17:57:40 +0800

    创建连接

--- webui/src/components/login/LoginBox.vue
+++ webui/src/components/login/LoginBox.vue
@@ -1,82 +1,82 @@
 <template>
-  <div v-loading="loading"
-       class="box-outer"
-       style="width: 420px;height:405px;padding-top:30px;padding-bottom: 8px;transition-duration: 0.5s">
-    <div class="title">
-      <div class="title-text">登录</div>
+  <div>
+    <div style="box-shadow: 1px 1px 4px rgba(50, 50, 50, 0.2);border-radius: 4px;">
+      <el-alert style="margin-bottom: 10px" v-if="error==='logout'" title="已断开连接" type="info" show-icon/>
+      <el-alert style="margin-bottom: 10px" v-else-if="error!==''" :title="error" type="error" show-icon/>
     </div>
-    <el-row>
-      <el-col :span="20" :offset="2">
-        <el-form
-            label-position="top"
-            label-width="100px"
-            :model="user"
-            style="max-width: 460px"
-        >
-          <!--          <el-form-item label="服务器">-->
-          <!--            <template #label>-->
-          <!--              服务器-->
-          <!--              <el-link :underline="false" style="color: #007BBB" @click="setting = true">-->
-          <!--                {{ server.address === "" ? "设置" : "切换" }}-->
-          <!--              </el-link>-->
-          <!--            </template>-->
-          <!--            <el-input v-model="serverShotCut" readonly style="cursor: pointer"/>-->
-          <!--          </el-form-item>-->
-          <el-form-item label="用户">
-            <el-input v-model="user.account"/>
-          </el-form-item>
-          <el-form-item label="密码">
-            <el-input v-model="user.password" show-password/>
-          </el-form-item>
-        </el-form>
-        <el-button type="primary" style="margin-top: 40px;width: 100%" @click="login">Login</el-button>
-      </el-col>
-    </el-row>
-    <el-divider style="margin-top: 50px;margin-bottom: 10px"/>
-    <div class="footer" style="text-align: center">
-      <div style="margin-bottom: 5px">
-        <el-link :underline="false" style="color: #007BBB;font-size: 13px"
-                 @click="setting = true">
-          连接设置
-        </el-link>
+    <div v-loading="loading"
+         class="box-outer"
+         style="width: 100%;height:405px;padding-top:30px;padding-bottom: 8px;transition-duration: 0.5s">
+      <div class="title" :style="running?'border-left: solid 8px #67C23A;':''">
+        <div class="title-text">{{ running ? "已连接" : "创建连接" }}</div>
       </div>
-      Connect to Hub {{ '<' }} <span style="color: #007BBB">{{ serverShotCut }}</span> {{ '>' }}
+      <el-row>
+        <el-col :span="20" :offset="2">
+          <el-form
+              label-position="top"
+              label-width="100px"
+              :model="user"
+              :disabled="running"
+              style="max-width: 460px"
+          >
+            <el-form-item label="用户">
+              <el-input v-model="user.account"/>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-if="!running" v-model="user.password" show-password/>
+              <el-input v-else v-model="displayPwd" show-password/>
+            </el-form-item>
+          </el-form>
+          <el-button type="primary" v-if="!running" style="margin-top: 40px;width: 100%" @click="start">连接</el-button>
+          <el-button type="danger" v-else style="margin-top: 40px;width: 100%" @click="stop">断开</el-button>
+        </el-col>
+      </el-row>
+      <el-divider style="margin-top: 50px;margin-bottom: 10px"/>
+      <div class="footer" style="text-align: center">
+        <div style="margin-bottom: 5px">
+          <el-link :underline="false" style="color: #007BBB;font-size: 13px"
+                   @click="setting = true">
+            连接设置
+          </el-link>
+        </div>
+        Connect to Hub {{ '<' }} <span style="color: #007BBB">{{ serverShotCut }}</span> {{ '>' }}
+      </div>
+      <el-drawer v-model="setting" title="客户端设置" :direction="'rtl'" :append-to-body="true" :show-close="false"
+                 custom-class="default-drawer">
+        <template #header>
+          <div class="title">
+            <div class="title-text">客户端设置</div>
+          </div>
+        </template>
+        <template #default>
+          <el-row :gutter="10">
+            <el-col :span="22" :offset="1">
+              <el-form
+                  label-position="top"
+                  label-width="100px"
+                  :model="server"
+              >
+                <el-form-item label="服务器地址">
+                  <el-input v-model="server.address"/>
+                </el-form-item>
+                <el-form-item label="服务器端口">
+                  <el-input v-model="server.port"/>
+                </el-form-item>
+                <el-form-item label="证书地址">
+                  <el-input v-model="server.cert"/>
+                </el-form-item>
+              </el-form>
+            </el-col>
+          </el-row>
+        </template>
+        <template #footer>
+          <div style="flex: auto">
+            <el-button @click="setting=false">取消</el-button>
+            <el-button type="primary" @click="saveConfig">确认</el-button>
+          </div>
+        </template>
+      </el-drawer>
     </div>
-    <el-drawer v-model="setting" title="客户端设置" :direction="'rtl'" :append-to-body="true" :show-close="false"
-               custom-class="default-drawer">
-      <template #header>
-        <div class="title">
-          <div class="title-text">客户端设置</div>
-        </div>
-      </template>
-      <template #default>
-        <el-row :gutter="10">
-          <el-col :span="22" :offset="1">
-            <el-form
-                label-position="top"
-                label-width="100px"
-                :model="server"
-            >
-              <el-form-item label="服务器地址">
-                <el-input v-model="server.address"/>
-              </el-form-item>
-              <el-form-item label="服务器端口">
-                <el-input v-model="server.port"/>
-              </el-form-item>
-              <el-form-item label="证书地址">
-                <el-input v-model="server.cert"/>
-              </el-form-item>
-            </el-form>
-          </el-col>
-        </el-row>
-      </template>
-      <template #footer>
-        <div style="flex: auto">
-          <el-button @click="setting=false">取消</el-button>
-          <el-button type="primary" @click="saveConfig">确认</el-button>
-        </div>
-      </template>
-    </el-drawer>
   </div>
 </template>
 
@@ -87,6 +87,9 @@ export default {
   name: "LoginBox",
   data() {
     return {
+      displayPwd: "******",
+      running: false,
+      error: "",
       setting: false,
       loading: false,
       version: "",
@@ -103,11 +106,45 @@ export default {
       }
     }
   },
-  mounted() {
+  async mounted() {
+    await this.check()
     this.loadConfig()
   },
   methods: {
-    login: function () {
+    check: function () {
+      this.loading = true
+      axios({
+        method: "get",
+        url: "/api/application/",
+        data: {}
+      }).then((res) => {
+        let response = res.data
+        console.log(response)
+        this.error = response.data.error
+        this.running = response.data.running
+        this.loading = false
+      }).catch((err) => {
+        this.loading = false
+        this.$utils.HandleError(err)
+      })
+    },
+    stop: function () {
+      this.loading = true
+      axios({
+        method: "get",
+        url: "/api/application/stop",
+        data: {}
+      }).then(() => {
+      }).catch((err) => {
+        this.$utils.HandleError(err)
+      }).finally(() => {
+        setTimeout(async () => {
+          await this.check()
+          this.loading = false
+        }, 1000)
+      })
+    },
+    start: function () {
       if (this.user.account === "") {
         this.$utils.Warning("提示", "用户名不能为空")
         return
@@ -134,6 +171,7 @@ export default {
           }
         }
       }).then(() => {
+        this.check()
         this.loading = false
       }).catch((err) => {
         this.loading = false
@@ -193,8 +231,8 @@ export default {
 
 <style scoped>
 .box-outer {
-  box-shadow: 1px 1px 5px rgba(50, 50, 50, 0.5);
-  border-radius: 10px;
+  box-shadow: 1px 1px 8px rgba(50, 50, 50, 0.2);
+  border-radius: 4px;
 }
 
 .footer {
