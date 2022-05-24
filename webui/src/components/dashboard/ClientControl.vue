d8843000a6d2fd97c876b8c2f2be33d4255d27dc
junqirao <jackrabbit872568318@aliyun.com>
2022-05-24 17:58:58 +0800

    update

---
+++ webui/src/components/dashboard/ClientControl.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-card shadow="always" body-style="padding:0">
+      <div class="title" style="margin-top: 20px;margin-bottom: 20px;">
+        <div class="title-text">客户端控制
+        </div>
+      </div>
+      <div style="padding: 20px">
+        <el-row :gutter="30">
+        </el-row>
+      </div>
+      <div style="font-size: 12px;color: #808080;text-align: right;padding: 5px 10px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ClientControl"
+}
+</script>
+
+<style scoped>
+
+</style>
