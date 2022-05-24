c572bf20193c6b41263f25f29ed5ca80fabb802a
junqirao <jackrabbit872568318@aliyun.com>
2022-05-24 17:17:27 +0800

    路由修改

--- webui/src/router.js
+++ webui/src/router.js
@@ -1,13 +1,24 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
 
 import LoginPage from "@/components/login/LoginPage";
+import DashboardPage from "@/components/dashboard/DashboardPage";
+//import axios from "axios";
 
 const routers = [
     {
         path: '/',
-        name: 'login',
-        alias: "/login",
+        name: 'default',
         component: LoginPage,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginPage,
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: DashboardPage,
     }
 ]
 
@@ -15,5 +26,28 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes: routers,
 })
+
+// router.beforeEach((to, from, next) => {
+//     if (to.name !== "loading") {
+//         axios({
+//             method: "get",
+//             url: "/api/application/",
+//             data: {}
+//         }).then((res) => {
+//             let response = res.data
+//             console.log(response)
+//             console.log(to)
+//             if (!response.data.running && to.name === "dashboard") {
+//                 router.push({path: "/login"})
+//             } else {
+//                 next()
+//             }
+//         }).catch(() => {
+//             router.push({path: "/"})
+//         })
+//     } else {
+//         next()
+//     }
+// })
 
 export default router
