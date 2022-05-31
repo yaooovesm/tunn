6afd7a4cb95a9c29ac786ab25056b9bc9a0b8c6a
junqirao <jackrabbit872568318@aliyun.com>
2022-05-31 11:30:34 +0800

    添加链接

--- webui/src/components/dashboard/LinkOverview.vue
+++ webui/src/components/dashboard/LinkOverview.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <el-row :gutter="30">
+    <el-row :gutter="30" v-loading="version===''">
       <el-col :span="24" style="font-size: 12px;color: #909399">
         <div style="text-align: center">
-          TunnClient
-          <span v-if="version===''" style="color: #E6A23C">未知版本</span>
-          <span v-else>V{{ version }}</span>
+          TunnHub
+          <span v-if="version===''" v-loading="true"></span>
+          <span v-else-if="version==='unknown'" style="color: #E6A23C">未知版本</span>
+          <span v-else>{{ version }}</span>
           <span v-if="develop">[开发版本]</span>
         </div>
         <div style="text-align: center;margin-top: 8px">
-          <el-link style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
+          <el-link href="https://github.com/yaooovesm/tunn/blob/master/doc/README_CN.md"
+                   style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
             文档
           </el-link>
           |
-          <el-link style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
+          <el-link href="https://gitee.com/jackrabbit872568318/tunn"
+                   style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
             Gitee
           </el-link>
           |
-          <el-link style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
+          <el-link href="https://github.com/yaooovesm/tunn"
+                   style="font-size: 12px;color: #007bbb;transform: translateY(-1px);">
             Github
           </el-link>
         </div>
@@ -51,7 +55,8 @@ export default {
         this.version = response.data.version
         this.develop = response.data.develop
       }).catch((err) => {
-        this.$utils.HandleError(err)
+        this.version = "unknown"
+        this.develop = false
       })
     },
   }
